d4cbff5d20b7bf6ac1d5036a0d03f1bd4fedada6
doctorwho1986 <doctorwho1986@foxmail.com>
2014-08-26 14:46:02 +0800

    SpringContextConfig

---
+++ practice/src/main/java/com/github/springMvc/config/SpringContextConfig.java
@@ -0,0 +1,10 @@
+package com.github.springMvc.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.github.springMvc.sprintbeans")
+public class SpringContextConfig {
+
+}
