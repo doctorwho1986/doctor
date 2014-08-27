100fc8515f6c2ae07e7e9279441f4b69b6d6c14f
doctorwho1986 <doctorwho1986@foxmail.com>
2014-08-27 20:43:10 +0800

    public Person getPerson();

--- practice/src/main/java/com/github/springMvc/example/service/IsayService.java
+++ practice/src/main/java/com/github/springMvc/example/service/IsayService.java
@@ -1,5 +1,8 @@
 package com.github.springMvc.example.service;
 
+import com.github.springMvc.example.domain.Person;
+
 public interface IsayService {
 	public String sayHello(String message);
+	public Person getPerson();
 }
