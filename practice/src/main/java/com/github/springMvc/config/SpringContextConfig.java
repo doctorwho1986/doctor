e7117e74e06171fdf4a264e68e05c9f10ce4b88a
doctorwho1986 <doctorwho1986@foxmail.com>
2014-08-27 18:03:08 +0800

    @ComponentScan({"com.github.springMvc.example.service.impl","com.github.springMvc.example.domain"})

--- practice/src/main/java/com/github/springMvc/config/SpringContextConfig.java
+++ practice/src/main/java/com/github/springMvc/config/SpringContextConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("com.github.springMvc.example.service.impl")
+@ComponentScan({"com.github.springMvc.example.service.impl","com.github.springMvc.example.domain"})
 public class SpringContextConfig {
 	
 }
