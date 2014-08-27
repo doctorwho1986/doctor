a7ddfd8a95205937892b8e36f355b3cfcafd540f
doctorwho1986 <doctorwho1986@foxmail.com>
2014-08-27 18:29:52 +0800

    @Service("isayService")

--- practice/src/main/java/com/github/springMvc/example/service/impl/IsayServiceImpl.java
+++ practice/src/main/java/com/github/springMvc/example/service/impl/IsayServiceImpl.java
@@ -1,14 +1,22 @@
 package com.github.springMvc.example.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import com.github.springMvc.example.domain.Person;
 import com.github.springMvc.example.service.IsayService;
 
 @Service("isayService")
 public class IsayServiceImpl implements IsayService{
-
+	@Autowired
+	@Qualifier("person")
+	Person person;
+	
 	@Override
 	public String sayHello(String message) {
+		person.setName("sd");
+		System.out.println(person.getName());
 		return "hello " + message;
 	}
 
