12d694c019321521fc15adf8a83782796ec91fb1
doctorwho1986 <doctorwho1986@foxmail.com>
2014-08-27 16:36:38 +0800

    hessianClientTest

---
+++ practice/src/test/java/com/github/springMvc/test/HessianClient.java
@@ -0,0 +1,22 @@
+package com.github.springMvc.test;
+
+
+import java.net.MalformedURLException;
+
+import org.junit.Test;
+
+import com.caucho.hessian.client.HessianProxyFactory;
+import com.github.springMvc.example.service.IsayService;
+
+public class HessianClient {
+
+	@Test
+	public void hessianClientTest() throws MalformedURLException {
+		String url = "http://localhost:8080/practice/remote/isayService";
+		HessianProxyFactory factory = new HessianProxyFactory();
+		IsayService api = (IsayService) factory.create(IsayService.class, url);
+
+		System.out.println(api.sayHello("weitang"));
+	}
+
+}
