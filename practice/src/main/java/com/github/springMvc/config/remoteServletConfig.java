0728a1b379532c466b41bb3a14b6ed0d62e25467
doctorwho1986 <doctorwho1986@foxmail.com>
2014-08-27 16:06:59 +0800

    remoteServletConfig

---
+++ practice/src/main/java/com/github/springMvc/config/remoteServletConfig.java
@@ -0,0 +1,10 @@
+package com.github.springMvc.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+@ImportResource("")
+public class remoteServletConfig {
+	
+}
