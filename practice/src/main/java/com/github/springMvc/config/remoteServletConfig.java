31ebe04f6154aebc51ba2e8e3ca73cb5c2071b68
doctorwho1986 <doctorwho1986@foxmail.com>
2014-08-27 16:26:47 +0800

    @ImportResource("classpath:/remote-servlet.xml")

--- practice/src/main/java/com/github/springMvc/config/remoteServletConfig.java
+++ practice/src/main/java/com/github/springMvc/config/remoteServletConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
 @Configuration
-@ImportResource("")
+@ImportResource("classpath:/remote-servlet.xml")
 public class remoteServletConfig {
 	
 }
