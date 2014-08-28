425339fc8fce2e7b544f889d213c62d525d6e766
doctorwho1986 <doctorwho1986@foxmail.com>
2014-08-28 17:15:28 +0800

    Constants

---
+++ practice/src/main/java/com/github/netexample/server/Constants.java
@@ -0,0 +1,8 @@
+package com.github.netexample.server;
+
+import java.io.File;
+
+public class Constants {
+	public static final String WEB_ROOT = 
+			System.getProperty("user.dir") + File.separator + "src/main/webroot";
+}
