a9269687811bcdaae1213202e51df6ca156cb9b2
doctorwho1986 <doctorwho1986@foxmail.com>
2014-09-09 10:02:31 +0800

    @ComponentScan({ 	"com.github.springMvc.controller" 	})

--- practice/src/main/java/com/github/springMvc/config/SpingMvcConfig.java
+++ practice/src/main/java/com/github/springMvc/config/SpingMvcConfig.java
@@ -4,7 +4,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("com.github.springMvc.controller")
+@ComponentScan({
+	"com.github.springMvc.controller"
+	})
 public class SpingMvcConfig {
 	
 }
