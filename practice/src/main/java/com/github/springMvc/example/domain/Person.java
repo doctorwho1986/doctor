0420e8976be896dacf9b9480b456564dbe80ecc3
doctorwho1986 <doctorwho1986@foxmail.com>
2014-09-12 15:12:08 +0800

    public enum Gender{ 		MAN,WOMAN 	}

--- practice/src/main/java/com/github/springMvc/example/domain/Person.java
+++ practice/src/main/java/com/github/springMvc/example/domain/Person.java
@@ -1,14 +1,26 @@
 package com.github.springMvc.example.domain;
 
 import java.io.Serializable;
+import java.lang.reflect.Field;
 
 import org.springframework.stereotype.Component;
 
 @Component("person")
 public class Person implements Serializable {
-	
+	public enum Gender{
+		MAN,WOMAN
+	}
 	private static final long serialVersionUID = -4205400683861996647L;
+	private String name =" hello doctor";
+	private Gender gender;
+	private String address;
 
+	public Person(String name,Gender gender,String address ){
+		this.name = name;
+		this.gender = gender;
+		this.address = address;
+	}
+	
 	public String getName() {
 		return name;
 	}
@@ -17,6 +29,37 @@ public class Person implements Serializable {
 		this.name = name;
 	}
 
-	private String name =" hello doctor";
+	public Gender getGender() {
+		return gender;
+	}
+
+	public void setGender(Gender gender) {
+		this.gender = gender;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	@Override
+	public String toString(){
+		StringBuffer stringBuffer = new StringBuffer();
+		stringBuffer.append("Person: ").append("{");
+		Field[] declaredFields = Person.class.getDeclaredFields();
+		try {
+			for (Field field : declaredFields) {
+				stringBuffer.append(field.getName()).append(" = ").append(field.get(this)).append(", ");
+			}
+		} catch (IllegalArgumentException | IllegalAccessException e) {
+			e.printStackTrace();
+		}
+		stringBuffer.deleteCharAt(stringBuffer.lastIndexOf(","));
+		return stringBuffer.toString();
+	}
 	
 }
+
