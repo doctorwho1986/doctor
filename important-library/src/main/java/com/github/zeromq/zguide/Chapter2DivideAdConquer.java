ebaf626e87b911dfcdb258482a3f7f0f8f94b4e4
doctorwho1986 <doctorwho1986@foxmail.com>
2014-09-18 18:35:14 +0800

    Chapter2DivideAdConquer

--- important-library/src/main/java/com/github/zeromq/zguide/Chapter2DivideAdConquer.java
+++ important-library/src/main/java/com/github/zeromq/zguide/Chapter2DivideAdConquer.java
@@ -2,9 +2,19 @@ package com.github.zeromq.zguide;
 
 /**
  * 
- * @author see http://zguide.zeromq.org/page:all#Divide and Conquer 
+ * @author see http://zguide.zeromq.org/page:all#Divide-and-Conquer
  *
  */
 public class Chapter2DivideAdConquer {
+
+	/*
+	 * 
+	 * Task ventilator in Java
+	 * Binds PUSH socket to tcp://localhost:5557
+	 * Sends batch of tasks to workers via that socket
+	 */
+	public void ventilator() {
+		
+	}
 
 }
