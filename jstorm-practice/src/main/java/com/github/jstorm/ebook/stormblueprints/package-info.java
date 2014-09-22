5bcb2b6b5a50f7136de438cfea9e8aae748db5cf
doctorwho1986 <doctorwho1986@foxmail.com>
2014-09-22 15:09:28 +0800

    Storm Blueprints: Patterns for Distributed Real-time Computation  *– March 26, 2014  * by P. Taylor Goetz (Author), Brian O'Neill (Author)

---
+++ jstorm-practice/src/main/java/com/github/jstorm/ebook/stormblueprints/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ * 
+ * Storm Blueprints: Patterns for Distributed Real-time Computation 
+ *– March 26, 2014
+ * by P. Taylor Goetz (Author), Brian O'Neill (Author)
+ * 
+ * 
+ */
+package com.github.jstorm.ebook.stormblueprints;
