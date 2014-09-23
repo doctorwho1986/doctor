734d02ffff4eedbe6c32d071bd036d58c5732bf1
doctorwho1986 <doctorwho1986@foxmail.com>
2014-09-23 20:58:53 +0800

    Chapter1

---
+++ java-practice/src/main/java/com/github/jdk8/ebook/functionalprogramminginjava/Chapter1.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2014 The doctor Authors
+ * https://github.com/doctorwho1986
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.jdk8.ebook.functionalprogramminginjava;
+
+/**
+ * @author doctor
+ *
+ * @date 2014年9月21日 下午4:20:26
+ */
+public class Chapter1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
