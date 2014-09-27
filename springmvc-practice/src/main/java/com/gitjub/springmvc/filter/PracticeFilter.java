9afdeceafd92c80f891ab22c28bea9925334692f
doctorwho1986 <doctorwho1986@foxmail.com>
2014-09-27 20:52:57 +0800

    PracticeFilter

---
+++ springmvc-practice/src/main/java/com/gitjub/springmvc/filter/PracticeFilter.java
@@ -0,0 +1,36 @@
+package com.gitjub.springmvc.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PracticeFilter implements Filter{
+	private static final Logger log = LoggerFactory.getLogger(PracticeFilter.class);
+	
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		log.info("{PracticeFilter :'init'}");
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		log.info("{PracticeFilter :'doFilter'}");
+		
+	}
+
+	@Override
+	public void destroy() {
+		log.info("{PracticeFilter :'destroy'}");
+		
+	}
+
+}
