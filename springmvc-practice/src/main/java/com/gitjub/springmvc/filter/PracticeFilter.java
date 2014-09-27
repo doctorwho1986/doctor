68a39ecfc3155e2beae6aeda28809cbdaae638c3
doctorwho1986 <doctorwho1986@foxmail.com>
2014-09-27 20:57:36 +0800

    chain.doFilter(request, response);

--- springmvc-practice/src/main/java/com/gitjub/springmvc/filter/PracticeFilter.java
+++ springmvc-practice/src/main/java/com/gitjub/springmvc/filter/PracticeFilter.java
@@ -24,6 +24,7 @@ public class PracticeFilter implements Filter{
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 		log.info("{PracticeFilter :'doFilter'}");
+		chain.doFilter(request, response);
 		
 	}
 
