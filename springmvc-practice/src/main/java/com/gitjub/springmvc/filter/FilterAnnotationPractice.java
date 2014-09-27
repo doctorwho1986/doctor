5da92e0dc25b4b1cc1e85a94550a03519a97d53f
doctorwho1986 <doctorwho1986@foxmail.com>
2014-09-28 00:17:08 +0800

    FilterAnnotationPractice

---
+++ springmvc-practice/src/main/java/com/gitjub/springmvc/filter/FilterAnnotationPractice.java
@@ -0,0 +1,37 @@
+package com.gitjub.springmvc.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@WebFilter(filterName = "filterAnnotationPractice",urlPatterns = {"/*"})
+public class FilterAnnotationPractice implements Filter {
+	private static final Logger log = LoggerFactory.getLogger(FilterAnnotationPractice.class);
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		log.info("{msg:'FilterAnnotationPractice  init'}");
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		log.info("{msg:'FilterAnnotationPractice  doFilter'}");
+		
+	}
+
+	@Override
+	public void destroy() {
+		log.info("{msg:'FilterAnnotationPractice  destroy'}");
+		
+	}
+
+}
