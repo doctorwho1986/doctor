9696e00d0663335a287c07c7787939e5efbd7540
doctorwho1986 <doctorwho1986@foxmail.com>
2014-09-28 22:30:20 +0800

    //在web.xml里面配置filter org.springframework.web.filter.CharacterEncodingFilter

--- springmvc-practice/src/main/java/com/gitjub/springmvc/controller/ControllerPractice1.java
+++ springmvc-practice/src/main/java/com/gitjub/springmvc/controller/ControllerPractice1.java
@@ -1,12 +1,17 @@
 package com.gitjub.springmvc.controller;
 
+import java.io.UnsupportedEncodingException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Controller
 public class ControllerPractice1 {
@@ -20,5 +25,16 @@ public class ControllerPractice1 {
 	@RequestMapping(value="/jsp",method={RequestMethod.GET,RequestMethod.POST})
 	public String jsp() {
 		return "jsptest";
+	}
+	
+	@RequestMapping(value="/form-post")
+	@ResponseBody
+	public String getEncode() {
+		String message = "";
+		HttpServletRequest request = ((ServletRequestAttributes)RequestContextHolder.getRequestAttributes()).getRequest();
+		//在web.xml里面配置filter org.springframework.web.filter.CharacterEncodingFilter
+		message = message + request.getCharacterEncoding();
+		
+		return message;
 	}
 }
