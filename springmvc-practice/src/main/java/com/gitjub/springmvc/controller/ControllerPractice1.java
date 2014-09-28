c75fe20f104c895659a10afedbb90ce466baf7e4
doctorwho1986 <doctorwho1986@foxmail.com>
2014-09-28 17:53:48 +0800

    ControllerPractice1

--- springmvc-practice/src/main/java/com/gitjub/springmvc/controller/ControllerPractice1.java
+++ springmvc-practice/src/main/java/com/gitjub/springmvc/controller/ControllerPractice1.java
@@ -1,5 +1,8 @@
 package com.gitjub.springmvc.controller;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,6 +13,6 @@ public class ControllerPractice1 {
 	@RequestMapping(value="/jsptest",method={RequestMethod.GET,RequestMethod.POST})
 	@ResponseBody
 	public String jspTest() {
-		return "hello jsp tester";
+		return " ! hello jsp tester,\n time now:" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
 	}
 }
