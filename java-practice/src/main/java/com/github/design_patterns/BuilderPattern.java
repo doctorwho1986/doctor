5c304f95aff461fe3c1c37d39a45ddf22dbb5c3e
doctorwho1986 <doctorwho1986@foxmail.com>
2014-10-08 16:06:59 +0800

    BuilderPattern

---
+++ java-practice/src/main/java/com/github/design_patterns/BuilderPattern.java
@@ -0,0 +1,20 @@
+package com.github.design_patterns;
+
+/**
+ * 
+ * @author
+ * 
+ * @link http://www.programcreek.com/2013/02/java-design-pattern-builder/
+ *       The key feature of Builder pattern is that it involves a step-by-step
+ *       process to build something, i.e., every produce will follow the same process
+ *       even though each step is different.
+ *
+ */
+public class BuilderPattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
