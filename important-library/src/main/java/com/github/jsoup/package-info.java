21a186635691d0563ff77fcf4110426714e8f627
doctorwho1986 <doctorwho1986@foxmail.com>
2014-10-13 14:28:02 +0800

    *  http://jsoup.org/cookbook/  *	jsoup Cookbook(中文版)  *  中文书籍：http://www.open-open.com/jsoup/

--- important-library/src/main/java/com/github/jsoup/package-info.java
+++ important-library/src/main/java/com/github/jsoup/package-info.java
@@ -1,7 +1,10 @@
 /**
  * @author 
  * @link http://jsoup.org/
- *
+ * 
+ *  http://jsoup.org/cookbook/
+ *	jsoup Cookbook(中文版)
+ *  中文书籍：http://www.open-open.com/jsoup/
  *
  */
 package com.github.jsoup;
