4345a4dbf504894b06c5448ac574dc2b5a36f69a
doctorwho1986 <doctorwho1986@foxmail.com>
2014-10-21 22:21:55 +0800

    testGetOpenAwardOfLeCai

---
+++ springmvc-practice/src/test/java/com/gitjub/springmvc/scheduler/Min11x5OpenAwardTest.java
@@ -0,0 +1,18 @@
+package com.gitjub.springmvc.scheduler;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:/springmvcPracticeConfig/spring-context.xml")
+public class Min11x5OpenAwardTest {
+
+	@Test
+	public void testGetOpenAwardOfLeCai() {
+		while(true){}
+	}
+
+}
