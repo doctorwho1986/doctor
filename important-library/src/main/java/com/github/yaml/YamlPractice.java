ef0f3ec2f036e66164a394e128bda2d39e37d02f
doctorwho1986 <doctorwho1986@foxmail.com>
2014-10-21 11:01:52 +0800

    YamlPractice

--- important-library/src/main/java/com/github/yaml/YamlPractice.java
+++ important-library/src/main/java/com/github/yaml/YamlPractice.java
@@ -3,16 +3,20 @@ package com.github.yaml;
 import java.io.InputStream;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.yaml.snakeyaml.Yaml;
 
 import com.alibaba.fastjson.JSON;
 
 
 public class YamlPractice {
+	private static final Logger LOG = LoggerFactory.getLogger(YamlPractice.class);
 
 	public static void main(String[] args) {
 		Yaml yaml = new Yaml();
 		InputStream inputStream = YamlPractice.class.getResourceAsStream("/importantLibraryYamlDefaultProp/jstorm.yaml");
+		@SuppressWarnings("unchecked")
 		Map<String, String> map = (Map<String, String>) yaml.load(inputStream);
 		System.out.println(map);
 		
@@ -20,7 +24,10 @@ public class YamlPractice {
 		
 		ConfigPractice configPractice =  yaml.loadAs(inputStream,ConfigPractice.class);
 		System.out.println(JSON.toJSONString(configPractice));
-
+		String dump = yaml.dump(configPractice);
+		System.out.println(dump);
+		LOG.info(dump);
+		LOG.info(JSON.toJSONString(configPractice));
 	}
 
 }
