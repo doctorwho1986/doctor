34286f4cac7dc5fa76731901915c9ee6882971d0
doctorwho1986 <doctorwho1986@foxmail.com>
2014-10-24 15:56:32 +0800

    KafkaJstormMain

---
+++ learning-jstorm/src/main/java/com/doctor/kafkajstrom/KafkaJstormMain.java
@@ -0,0 +1,81 @@
+package com.doctor.kafkajstrom;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.doctor.kafkajstrom.util.ConfigUtil;
+
+import nl.minvenj.nfi.storm.kafka.KafkaSpout;
+import backtype.storm.Config;
+import backtype.storm.LocalCluster;
+import backtype.storm.StormSubmitter;
+import backtype.storm.generated.AlreadyAliveException;
+import backtype.storm.generated.InvalidTopologyException;
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.TopologyBuilder;
+import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.tuple.Tuple;
+
+public class KafkaJstormMain {
+	private static final Logger LOG = LoggerFactory.getLogger(KafkaJstormMain.class);
+	
+	public static void main(String[] args) {
+
+		TopologyBuilder builder = new TopologyBuilder();
+
+		builder.setSpout("kafka-spout", new KafkaSpout());
+		builder.setBolt("kafka-bolt", new KafkaBolt()).allGrouping("kafka-spout");
+		
+
+		Properties props = new Properties();
+		try {
+			props.load(KafkaJstormMain.class.getResourceAsStream("learningJstormDefaultPop/kafka-config.properties"));
+		} catch (IOException e) {
+			throw new IllegalArgumentException("learningJstormDefaultPop/kafka-config.properties io error");
+		}
+		
+		Config config = ConfigUtil.of(props);
+		
+//		LocalCluster cluster = new LocalCluster();
+//		cluster.submitTopology("my-topology", config, builder.createTopology());
+		
+		try {
+			StormSubmitter.submitTopologyWithProgressBar("kafka-topology", config, builder.createTopology());
+		} catch (AlreadyAliveException | InvalidTopologyException e) {
+			LOG.error("{msg:'StormSubmitter.submitTopologyWithProgressBar error  {}'}",e);
+		}
+	}
+
+	public static class KafkaBolt extends BaseRichBolt {
+		
+		private static final Logger LOG = LoggerFactory.getLogger(KafkaBolt.class);
+		private static final long serialVersionUID = 1L;
+
+		@Override
+		public void prepare(Map stormConf, TopologyContext context, OutputCollector collector) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void execute(Tuple input) {
+			String logMessage = input.getStringByField("bytes");
+			System.out.println(logMessage);
+			LOG.info("{msg:'{}  获得数据  {}'}", Thread.currentThread().getName() + " -- " + KafkaBolt.class.getName(), logMessage);
+
+		}
+
+		@Override
+		public void declareOutputFields(OutputFieldsDeclarer declarer) {
+			// TODO Auto-generated method stub
+
+		}
+
+	}
+}
