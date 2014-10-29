20d57b51095ae35690f0c4a3b573bac499066723
doctorwho1986 <doctorwho1986@foxmail.com>
2014-10-29 18:56:36 +0800

    LocalJstormMain

--- learning-jstorm/src/main/java/com/doctor/kafkajstrom/LocalJstormMain.java
+++ learning-jstorm/src/main/java/com/doctor/kafkajstrom/LocalJstormMain.java
@@ -1,16 +1,10 @@
 package com.doctor.kafkajstrom;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.lang.RandomStringUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
