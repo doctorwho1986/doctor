89ad3da18798a355ed4cc4b130b035d5ffa2f6ba
doctorwho1986 <doctorwho1986@foxmail.com>
2014-11-03 17:39:32 +0800

    test_Request_execution

--- test-framework/src/main/java/com/doctor/httpclient/Chapter1.java
+++ test-framework/src/main/java/com/doctor/httpclient/Chapter1.java
@@ -2,11 +2,13 @@ package com.doctor.httpclient;
 
 import java.io.IOException;
 
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.junit.Test;
 
 public class Chapter1 {
@@ -15,7 +17,10 @@ public class Chapter1 {
 	public void test_Request_execution() throws ClientProtocolException, IOException{
 		HttpClient httpClient = HttpClients.createDefault();
 		HttpGet httpGet = new HttpGet("http://www.baidu.com");
-		HttpResponse response = httpClient.execute(httpGet);
-		System.out.println(response);
+		HttpResponse httpResponse = httpClient.execute(httpGet);
+		System.out.println(httpResponse);
+		HttpEntity httpEntity = httpResponse.getEntity();
+		String string = EntityUtils.toString(httpEntity);
+		System.out.println(string);
 	}
 }
