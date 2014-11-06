872304e288470e1c4438296f0bd20b468bd95c22
doctorwho1986 <doctorwho1986@foxmail.com>
2014-11-06 14:02:50 +0800

    test_Composite_Java_API_Design

--- test-framework/src/main/java/com/doctor/moco/apiexample/ApiExample.java
+++ test-framework/src/main/java/com/doctor/moco/apiexample/ApiExample.java
@@ -1,13 +1,21 @@
 package com.doctor.moco.apiexample;
 
+import org.apache.http.Header;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpVersion;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.fluent.Content;
 import org.apache.http.client.fluent.Request;
+import org.apache.http.client.methods.RequestBuilder;
 
 import static org.junit.Assert.assertThat;
-
 import static org.hamcrest.core.Is.is;
+
 import org.junit.Test;
 
+import static org.hamcrest.core.IsEqual.equalTo;
+
+import com.github.dreamhead.moco.HttpProtocolVersion;
 import com.github.dreamhead.moco.HttpServer;
 import com.github.dreamhead.moco.Moco;
 import com.github.dreamhead.moco.Runner;
@@ -30,4 +38,29 @@ public class ApiExample {
 			assertThat(content.toString(), is("doctor"));
 		});
 	}
+	
+	@Test
+	public void test_Composite_Java_API_Design() throws Exception{
+		HttpServer httpserver = Moco.httpserver(12306);
+//		httpserver.request(Moco.and(by(uri("/target")), by(version(VERSION_1_0)))).response(with(text("foo")), header("Content-Type", "text/html"));
+		httpserver.request(Moco.and(
+				Moco.by(Moco.uri("/a")
+						))).response(Moco.with("hello-doctor-who"),
+								Moco.header("who", "doctor"),
+								Moco.cookie("no", "me"));
+		Runner.running(httpserver, () -> {
+			
+			HttpResponse httpResponse = Request.Get("http://localhost:12306/a").execute().returnResponse();
+			assertThat(HttpVersion.HTTP_1_1, equalTo(httpResponse.getProtocolVersion()));
+			Header[] headers = httpResponse.getAllHeaders();
+			for (Header header : headers) {
+				System.out.println(header.getName() + " : " + header.getValue());
+			}
+			
+		}); 
+		
+	
+	}
+	
+	
 }
