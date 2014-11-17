9d93181fc3c6d7f8fadc9fface259f5db8a8f5bc
doctorwho1986 <doctorwho1986@foxmail.com>
2014-11-17 19:32:17 +0800

    StringConverterPractice

--- important-library/src/main/java/com/doctor/beanutil/StringConverterPractice.java
+++ important-library/src/main/java/com/doctor/beanutil/StringConverterPractice.java
@@ -11,6 +11,16 @@ public class StringConverterPractice {
 		System.out.println(stringConverter.convert(String.class, null));
 		Preconditions.checkArgument(stringConverter.convert(String.class, null)==null);
 		Preconditions.checkArgument(stringConverter.convert(String.class, "ss").equals("ss"));
+		Preconditions.checkArgument(stringConverter.convert(String.class, 12).equals("12"));
+		Preconditions.checkArgument(stringConverter.convert(String.class, 12.000F).equals("12.0"));
+		Preconditions.checkArgument(stringConverter.convert(String.class, 12.001F).equals("12.001"));
+		
+		try {
+			Preconditions.checkArgument(stringConverter.convert(Integer.class, "12").equals(Integer.valueOf(12)));
+		} catch (Exception e) {
+			 
+			System.out.println(e.getMessage());
+		}
 	}
 
 }
