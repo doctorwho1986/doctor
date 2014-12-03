a23da5b150b4e9bee5e9341405deaf72bd9a612f
doctorwho1986 <doctorwho1986@foxmail.com>
2014-12-03 16:39:17 +0800

    ObserverPatternWithSpringEvents

--- springmvc-practice/src/test/java/spring/core/pracitce/ObserverPatternWithSpringEvents.java
+++ springmvc-practice/src/test/java/spring/core/pracitce/ObserverPatternWithSpringEvents.java
@@ -3,9 +3,6 @@ package spring.core.pracitce;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
