6f4493024125f156687a1e1e77b8e09e3ce9cd92
doctorwho1986 <doctorwho1986@foxmail.com>
2014-12-16 19:09:14 +0800

    hbase创建表

---
+++ HBase-practice/src/main/java/com/doctor/localhost/HBaseConnector.java
@@ -0,0 +1,40 @@
+package com.doctor.localhost;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.MasterNotRunningException;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.ZooKeeperConnectionException;
+import org.apache.hadoop.hbase.client.HBaseAdmin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Preconditions;
+
+public class HBaseConnector {
+	private static final Logger log = LoggerFactory.getLogger(HBaseConnector.class);
+	
+	public static void main(String[] args) throws MasterNotRunningException, ZooKeeperConnectionException, IOException {
+		final String tableName = "docotr_hbase";
+		Configuration configuration = HBaseConfiguration.create();
+		configuration.addResource("hbasePracticeConfigDefault/hbase-site.xml");
+		HBaseAdmin hBaseAdmin = new HBaseAdmin(configuration);
+		if (hBaseAdmin.isTableAvailable(tableName)) {
+			log.info("'table exist': {}",tableName);
+			hBaseAdmin.disableTable(tableName);
+			hBaseAdmin.deleteTable(tableName);
+		}
+		HTableDescriptor desc = new HTableDescriptor(TableName.valueOf("docotr_hbase"));
+		desc.addFamily(new HColumnDescriptor("cf1"));
+		desc.addFamily(new HColumnDescriptor("cf2"));
+		hBaseAdmin.createTable(desc );
+		boolean tableAvailable = hBaseAdmin.isTableAvailable("docotr_hbase");
+		Preconditions.checkArgument(tableAvailable);
+		log.info("{}",tableAvailable);
+		hBaseAdmin.close();
+	}
+}
---
+++ HBase-practice/src/main/java/com/doctor/localhost/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * @author doctor
+ *
+ * @time   2014年12月16日 下午5:50:59
+ */
+package com.doctor.localhost;
