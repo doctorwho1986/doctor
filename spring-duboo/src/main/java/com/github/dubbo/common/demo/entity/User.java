4f01463eb3a694f2f13d69ff658d968ee143618d
doctorwho1986 <doctorwho1986@foxmail.com>
2014-12-21 10:41:01 +0800

    实体类

---
+++ spring-duboo/src/main/java/com/github/dubbo/common/demo/entity/User.java
@@ -0,0 +1,37 @@
+package com.github.dubbo.common.demo.entity;
+
+/**
+ * @author doctor
+ *
+ * @since 2014年12月21日 上午10:33:21
+ */
+public class User {
+	private String name;
+	private Integer age;
+	private String sex;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+}
