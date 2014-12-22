8b6c4de85a8b647ca3bd433a7991de10342accea
doctorwho1986 <doctorwho1986@foxmail.com>
2014-12-22 22:17:26 +0800

    @author doctor

--- spring-duboo/src/test/java/com/github/dubbo/common/demo/service/MergeServiceWrongTest.java
+++ spring-duboo/src/test/java/com/github/dubbo/common/demo/service/MergeServiceWrongTest.java
@@ -12,7 +12,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * http://alibaba.github.io/dubbo-doc-static/User+Guide-zh.htm#UserGuide-zh-%E6%9C%8D%E5%8A%A1%E5%88%86%E7%BB%84
  * dubbo 的文档指出：按组合并返回结果，比如菜单服务，接口一样，但有多种实现，用group区分，
  * 现在消费方需从每种group中调用一次返回结果，合并结果返回，这样就可以实现聚合菜单项。
- * @author cui
+ * @author  doctor
  *
  * @time   2014年12月22日 下午6:01:32
  */
