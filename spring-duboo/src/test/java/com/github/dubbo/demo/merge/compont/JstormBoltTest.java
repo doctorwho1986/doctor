d681c9ddceb982391af166ca28128ca34071d2f2
doctorwho1986 <doctorwho1986@foxmail.com>
2014-12-25 10:16:08 +0800

    注：模拟的是一个jvm启动多个dubbo应用（端口一样不会报错）。和集群有区别，集群是一台机器上部署多个相同的dubbo，但端口不一样（一样报错）  * ，或者不同机器上部署。

--- spring-duboo/src/test/java/com/github/dubbo/demo/merge/compont/JstormBoltTest.java
+++ spring-duboo/src/test/java/com/github/dubbo/demo/merge/compont/JstormBoltTest.java
@@ -12,6 +12,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.github.dubbo.common.demo.service.MergeService;
 /**
  * 模拟jstrom bolt ,bolt一台机器上多实例(有可能实例分到不同机器上），分组聚合实验失败
+ * 注：模拟的是一个jvm启动多个dubbo应用（端口一样不会报错）。和集群有区别，集群是一台机器上部署多个相同的dubbo，但端口不一样（一样报错）
+ * ，或者不同机器上部署。
  * @author doctor
  *
  * @time   2014年12月23日 上午10:22:07
@@ -34,13 +36,13 @@ public class JstormBoltTest {
 	
 	@After
 	public void destroy(){
-//		while(true){
-//			
-//		}
-		jstormBolt1.cleanup();
-		jstormBolt2.cleanup();
-		
-		consumerContext.close();
+		while(true){
+			
+		}
+//		jstormBolt1.cleanup();
+//		jstormBolt2.cleanup();
+//		
+//		consumerContext.close();
 	}
 	@Test
 	public void test() {
