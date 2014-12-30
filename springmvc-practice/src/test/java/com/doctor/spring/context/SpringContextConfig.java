a95166cbafcbeeadfe2b32b5538dc75e7ad92cd4
doctorwho1986 <doctorwho1986@foxmail.com>
2014-12-30 22:49:46 +0800

    非spring管理的类实例如何托管给spring容器管理

---
+++ springmvc-practice/src/test/java/com/doctor/spring/context/SpringContextConfig.java
@@ -0,0 +1,15 @@
+package com.doctor.spring.context;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author doctor
+ *
+ * @since 2014年12月30日 下午10:21:43
+ */
+@Configuration
+@ComponentScan(basePackages={"com.doctor.spring.context"})
+public class SpringContextConfig {
+
+}
