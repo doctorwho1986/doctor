b1b661d2c256b85f46f1733e6d534625c60fd2b2
doctorwho1986 <doctorwho1986@foxmail.com>
2015-01-04 23:34:26 +0800

    注入ｓｐｒｉｎｇ容器

--- springmvc-practice/src/test/java/com/doctor/spring/context/lifecycle/LifecycleAnnotherBean.java
+++ springmvc-practice/src/test/java/com/doctor/spring/context/lifecycle/LifecycleAnnotherBean.java
@@ -4,13 +4,19 @@ import javax.annotation.PostConstruct;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 @Component("lifecycleAnnotherBean")
 public class LifecycleAnnotherBean {
 	private static final Logger log = LoggerFactory.getLogger(LifecycleAnnotherBean.class);
+
+	@Autowired
+	private ApplicationContext applicationContext;
 	@PostConstruct
 	public void postConstruct(){
 		log.info("@PostConstruct:'postConstruct()'");
+		log.info(applicationContext.toString());
 	}
 }
