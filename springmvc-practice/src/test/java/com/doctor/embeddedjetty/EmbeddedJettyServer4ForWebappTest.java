31e40c9916acb41cb83a68d453c6f326af209598
doctorwho1986 <doctorwho1986@foxmail.com>
2015-01-07 14:38:49 +0800

    视图  测试

--- springmvc-practice/src/test/java/com/doctor/embeddedjetty/EmbeddedJettyServer4ForWebappTest.java
+++ springmvc-practice/src/test/java/com/doctor/embeddedjetty/EmbeddedJettyServer4ForWebappTest.java
@@ -1,7 +1,8 @@
 package com.doctor.embeddedjetty;
 
-import static org.junit.Assert.*;
 
+import org.apache.http.client.fluent.Request;
+import org.apache.http.client.fluent.Response;
 import org.junit.Test;
 
 public class EmbeddedJettyServer4ForWebappTest {
@@ -10,9 +11,12 @@ public class EmbeddedJettyServer4ForWebappTest {
 	public void test() throws Throwable{
 		EmbeddedJettyServer4ForWebapp server = new EmbeddedJettyServer4ForWebapp(8789,"/embeddedJettyServer4ForWebapp");
 		server.start();
-		while(true){
-			
-		}
+		Response response = Request.Get("http://localhost:8789/jetty/test.html").execute();
+		System.out.println(response.returnContent().asString());
+		
+		response = Request.Get("http://localhost:8789/jetty/test.json").execute();
+		System.out.println(response.returnContent().asString());
+		server.stop();
 	}
 
 }
--- springmvc-practice/src/test/java/com/doctor/embeddedjetty/EmbeddedJettyServerController.java
+++ springmvc-practice/src/test/java/com/doctor/embeddedjetty/EmbeddedJettyServerController.java
@@ -12,12 +12,12 @@ import org.springframework.web.servlet.ModelAndView;
 public class EmbeddedJettyServerController {
 
 	@RequestMapping({"/jetty/test.html","/jetty/test.json"})
-	public ModelAndView test() {
+	public ModelAndView test2() {
 		Map<String, String> map = new HashMap<>();
 		map.put("test", "json");
 		map.put("test-html", "html");
 		map.put("what", "what");
-		ModelAndView modelAndView = new ModelAndView("/test");
+		ModelAndView modelAndView = new ModelAndView("jettytest/test");
 		modelAndView.addObject(map);
 		return modelAndView;
 		
