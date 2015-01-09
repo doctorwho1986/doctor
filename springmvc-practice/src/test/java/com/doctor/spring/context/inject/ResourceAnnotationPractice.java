0a8478a4eb77c8ec77499a075be21a18a35cc393
doctorwho1986 <doctorwho1986@foxmail.com>
2015-01-09 17:05:49 +0800

    1.注释配置和 XML 配置的适用场合

--- springmvc-practice/src/test/java/com/doctor/spring/context/inject/ResourceAnnotationPractice.java
+++ springmvc-practice/src/test/java/com/doctor/spring/context/inject/ResourceAnnotationPractice.java
@@ -15,6 +15,16 @@ import org.springframework.context.annotation.Configuration;
  *           一般情况下，我们无需使用类似于 @Resource(type=Car.class) 的注释方式，
  *           因为 Bean 的类型信息可以通过 Java 反射从代码中获取。
  * 
+ *           1.注释配置和 XML 配置的适用场合
+ * 
+ *           是否有了这些 IOC 注释，我们就可以完全摒除原来 XML 配置的方式呢？答案是否定的。有以下几点原因：
+ * 
+ *           注释配置不一定在先天上优于 XML 配置。如果 Bean 的依赖关系是固定的，（如 Service 使用了哪几个 DAO 类），这种配置信息不会在部署时发生调整，那么注释配置优于 XML 配置；反之如果这种依赖关系会在部署时发生调整，XML 配置显然又优于注释配置，因为注释是对 Java 源代码的调整，您需要重新改写源代码并重新编译才可以实施调整。
+ *           如果 Bean 不是自己编写的类（如 JdbcTemplate、SessionFactoryBean 等），注释配置将无法实施，此时 XML 配置是唯一可用的方式。
+ *           注释配置往往是类级别的，而 XML 配置则可以表现得更加灵活。比如相比于 @Transaction 事务注释，使用 aop/tx 命名空间的事务配置更加灵活和简单。
+ * 
+ *           所以在实现应用中，我们往往需要同时使用注释配置和 XML 配置，对于类级别且不会发生变动的配置可以优先考虑注释配置；而对于那些第三方类以及容易发生调整的配置则应优先考虑使用 XML 配置。Spring 会在具体实施 Bean 创建和 Bean 注入之前将这两种配置方式的元信息融合在一起。
+ * 
  * @author doctor
  *
  * @time 2015年1月9日 下午4:54:56
@@ -30,7 +40,7 @@ public class ResourceAnnotationPractice {
 		for (String beanName : context.getBeanDefinitionNames()) {
 			System.out.println(beanName);
 		}
-		
+
 		context.close();
 	}
 
