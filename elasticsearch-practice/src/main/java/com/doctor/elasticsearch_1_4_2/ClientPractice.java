95564fa2703b31025744af67c0501a5acb5b89e0
doctorwho1986 <doctorwho1986@foxmail.com>
2015-01-16 15:50:02 +0800

    //从配置文件内获得配置 资源根路径下面的配置

--- elasticsearch-practice/src/main/java/com/doctor/elasticsearch_1_4_2/ClientPractice.java
+++ elasticsearch-practice/src/main/java/com/doctor/elasticsearch_1_4_2/ClientPractice.java
@@ -18,13 +18,22 @@ public class ClientPractice {
 		Node node1 = NodeBuilder.nodeBuilder().clusterName("embedded-1").client(true).local(true).node();//启动
 		Node node2 = NodeBuilder.nodeBuilder().clusterName("embedded-2").client(true).local(true).node();//启动
 		
-		Client client = node1.client();
+		Client client1 = node1.client();
 		Client client2 = node2.client();
 		
 		
 		TimeUnit.SECONDS.sleep(3);
 		node1.close();
 		node2.close();
+		
+		
+		//从配置文件内获得配置 资源根路径下面的配置
+		System.out.println("------------从配置文件内获得配置------------");
+		Node node = NodeBuilder.nodeBuilder().node();
+		Client client = node.client();
+		TimeUnit.SECONDS.sleep(2);
+		
+		node.close();
 	}
 
 }
