145071656a1953c502b732b17ebd7524ea238276
doctorwho1986 <doctorwho1986@foxmail.com>
2015-01-19 10:56:39 +0800

    * Mastering ElasticSearch  * Copyright © 2013 Packt Publishing

---
+++ elasticsearch-practice/src/main/java/com/doctor/elasticsearch_1_4_2/mastering_elasticSearch_book/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Mastering ElasticSearch
+ * Copyright © 2013 Packt Publishing
+
+ * @author doctor
+ *
+ * @time   2015年1月19日 上午10:55:06
+ */
+package com.doctor.elasticsearch_1_4_2.mastering_elasticSearch_book;
