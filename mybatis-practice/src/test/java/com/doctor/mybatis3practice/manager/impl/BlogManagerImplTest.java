f7c52568b4072e7ad24e93ec4cb9a4711643989e
doctorwho1986 <doctorwho1986@foxmail.com>
2015-01-21 22:07:04 +0800

    BlogManagerImplTest

---
+++ mybatis-practice/src/test/java/com/doctor/mybatis3practice/manager/impl/BlogManagerImplTest.java
@@ -0,0 +1,47 @@
+package com.doctor.mybatis3practice.manager.impl;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.core.IsEqual.*;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.doctor.mybatis3practice.domain.Blog;
+import com.doctor.mybatis3practice.manager.BlogManager;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(value = { "classpath:mybatisPractice-config/spring-context.xml" })
+public class BlogManagerImplTest {
+
+	@Resource
+	private BlogManager blogManager;
+
+	@Test
+	public void testQueryBlog() {
+		Blog blog = new Blog();
+		blog.setAuthorId(11L);
+		blog.setTitle("who know");
+
+		boolean b = blogManager.insertBlog(blog);
+		assertTrue(b);
+
+		Blog blogFromDb = blogManager.queryBlog(blog.getId());
+		assertNotNull(blog);
+		assertThat(blog.toString(), equalTo(blogFromDb.toString()));
+	}
+
+	@Test
+	public void testInsertBlog() {
+		Blog blog = new Blog();
+		blog.setAuthorId(11L);
+		blog.setTitle("who know");
+
+		boolean b = blogManager.insertBlog(blog);
+		assertTrue(b);
+	}
+
+}
