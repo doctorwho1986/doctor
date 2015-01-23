84138a1e3bfe85ef6cdec12c71d52adc4525b6ce
doctorwho1986 <doctorwho1986@foxmail.com>
2015-01-23 18:18:32 +0800

    * 自定义Annotation，得到注解类中Annotation设定的注解值  * 即：遍历自定义Annotation中的方法，反射执行方法，结果就是  * 对应的注解值。  *

---
+++ springmvc-practice/src/test/java/com/doctor/spring/core/AnnotationPractice.java
@@ -0,0 +1,57 @@
+package com.doctor.spring.core;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.reflect.Method;
+
+/**
+ * 自定义Annotation，得到注解类中Annotation设定的注解值
+ * 即：遍历自定义Annotation中的方法，反射执行方法，结果就是
+ * 对应的注解值。
+ * 
+ * @author doctor
+ *
+ * @time 2015年1月23日 下午6:15:31
+ */
+
+public class AnnotationPractice {
+
+	public static void main(String[] args) throws ReflectiveOperationException {
+		MyAnnotation myAnnotation = AnnotationP.class.getAnnotation(MyAnnotation.class);
+		System.out.println(myAnnotation);
+
+		for (Method method : myAnnotation.annotationType().getDeclaredMethods()) {
+			if (!method.isAccessible()) {
+				method.setAccessible(true);
+			}
+			Object invoke = method.invoke(myAnnotation);
+			System.out.println("invoke methd " + method.getName() + " result:" + invoke);
+			if (invoke.getClass().isArray()) {
+				Object[] temp = (Object[]) invoke;
+				for (Object o : temp) {
+					System.out.println(o);
+				}
+			}
+		}
+
+	}
+
+	@Retention(RetentionPolicy.RUNTIME)
+	@Target(ElementType.TYPE)
+	@Documented
+	private @interface MyAnnotation {
+		String value() default "";
+
+		int num() default 100;
+
+		String[] address() default {};
+	}
+
+	@MyAnnotation(value = "AnnotationP", num = 12, address = { "1", "2" })
+	private static class AnnotationP {
+
+	}
+}
