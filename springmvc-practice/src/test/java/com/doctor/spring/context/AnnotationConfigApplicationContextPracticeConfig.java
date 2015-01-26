e64cbe72ca4ff9b7db5ef8c7a27bab9433a50871
doctorwho1986 <doctorwho1986@foxmail.com>
2015-01-26 11:28:53 +0800

    启动方便调试跟踪一些功能

---
+++ springmvc-practice/src/test/java/com/doctor/spring/context/AnnotationConfigApplicationContextPracticeConfig.java
@@ -0,0 +1,10 @@
+package com.doctor.spring.context;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.ComponentScan;
+
+@Configurable
+@ComponentScan({"com.doctor.spring.context"})
+public class AnnotationConfigApplicationContextPracticeConfig {
+
+}
