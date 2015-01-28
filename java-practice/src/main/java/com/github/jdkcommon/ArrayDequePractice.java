9555ff8d935ca1a5b88a32e8379a3b9e3191d42e
doctorwho1986 <doctorwho1986@foxmail.com>
2015-01-28 09:54:39 +0800

    ArrayDeque

---
+++ java-practice/src/main/java/com/github/jdkcommon/ArrayDequePractice.java
@@ -0,0 +1,15 @@
+package com.github.jdkcommon;
+/**
+ * {@link ArrayDeque ArrayDeque}
+ * @author doctor
+ *
+ * @time   2015年1月28日 上午9:50:07
+ */
+public class ArrayDequePractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
