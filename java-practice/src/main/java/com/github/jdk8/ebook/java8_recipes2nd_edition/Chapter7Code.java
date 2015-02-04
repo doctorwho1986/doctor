f2e96e92a1848976ac8b99d2671deb5003427bc6
doctorwho1986 <doctorwho1986@foxmail.com>
2015-02-04 17:33:28 +0800

    Chapter7Code

---
+++ java-practice/src/main/java/com/github/jdk8/ebook/java8_recipes2nd_edition/Chapter7Code.java
@@ -0,0 +1,15 @@
+package com.github.jdk8.ebook.java8_recipes2nd_edition;
+/**
+ * p185
+ * @author doctor
+ *
+ * @time   2015年2月4日 下午5:32:33
+ */
+public class Chapter7Code {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
