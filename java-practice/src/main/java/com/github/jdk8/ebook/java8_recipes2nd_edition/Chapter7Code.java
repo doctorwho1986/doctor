bc70a254edd7bcbae2b62c060a37b85dba316673
doctorwho1986 <doctorwho1986@foxmail.com>
2015-02-04 21:40:03 +0800

    Chapter7Code

--- java-practice/src/main/java/com/github/jdk8/ebook/java8_recipes2nd_edition/Chapter7Code.java
+++ java-practice/src/main/java/com/github/jdk8/ebook/java8_recipes2nd_edition/Chapter7Code.java
@@ -1,4 +1,10 @@
 package com.github.jdk8.ebook.java8_recipes2nd_edition;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * p185
  * @author doctor
@@ -8,7 +14,23 @@ package com.github.jdk8.ebook.java8_recipes2nd_edition;
 public class Chapter7Code {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//map stream
+		Map<String, String> map = new HashMap<>();
+		map.put("name", "doctor");
+		map.put("sex", "man");
+		map.put("age", "20000");
+		System.out.println("map stream------------");
+		map.forEach((k,v)-> System.out.println(k+":"+v));
+		
+		//list stream
+		List<String> list = Arrays.asList("name","sex","age","address");
+		System.out.println("list stream-----------");
+		list.parallelStream().forEach(System.out::println);
+		
+		//array stream
+		int[] array = {1,33,55,66,77,88,99,100};
+		System.out.println("array stream-----------------");
+		Arrays.stream(array).forEach(System.out::println);
 
 	}
 
