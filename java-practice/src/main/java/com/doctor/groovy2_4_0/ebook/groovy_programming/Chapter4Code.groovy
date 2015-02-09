c5c2935365bc7ad1257c9c29f5324e80176577ae
doctorwho1986 <doctorwho1986@foxmail.com>
2015-02-09 23:45:25 +0800

    Chapter4Code

---
+++ java-practice/src/main/java/com/doctor/groovy2_4_0/ebook/groovy_programming/Chapter4Code.groovy
@@ -0,0 +1,24 @@
+package com.doctor.groovy2_4_0.ebook.groovy_programming
+
+/**
+ * @author doctor
+ *
+ * @since 2015年2月9日 下午10:58:30
+ */
+class Chapter4Code {
+
+	static main(args) {
+		def list =[1,2,3,4]
+		println list
+		list = list-2
+		println list
+		list+=5
+		println  list
+		list << 88
+		println  list
+		list -88
+		println list
+		println list-88
+	}
+
+}
