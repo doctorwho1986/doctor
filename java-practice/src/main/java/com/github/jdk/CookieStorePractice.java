c9547ff19e22607a139a820f7085cc974e0bcc22
doctorwho1986 <doctorwho1986@foxmail.com>
2015-02-12 15:45:55 +0800

    CookieStorePractice   @see http://examples.javacodegeeks.com/core-java/net/cookiestore-net/java-net-cookiestore-example/

--- java-practice/src/main/java/com/github/jdk/CookieStorePractice.java
+++ java-practice/src/main/java/com/github/jdk/CookieStorePractice.java
@@ -5,6 +5,11 @@ import java.net.CookieHandler;
 import java.net.CookieManager;
 import java.net.URL;
 import java.net.URLConnection;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+import org.apache.commons.io.IOUtils;
 /**
  * @see http://examples.javacodegeeks.com/core-java/net/cookiestore-net/java-net-cookiestore-example/
  * @author doctor
@@ -19,6 +24,11 @@ public class CookieStorePractice {
 		
 		URL netUrl = new URL(url);
 		URLConnection urlConnection = netUrl.openConnection();
+		System.out.println("getContentType:" + urlConnection.getContentType());
+		System.out.println("getDate:" + LocalDateTime.ofInstant(Instant.ofEpochMilli(urlConnection.getDate()), ZoneId.of("Asia/Shanghai")));
+		
+		System.out.println("content:" + IOUtils.toString(urlConnection.getInputStream()));
+		
 		Object content = urlConnection.getContent();
 		System.out.println(content.toString());
 		System.out.println("----------------------------------------");
