8ee053cb9d40dd3e458ffe86bc39054841d7f3b7
doctorwho1986 <doctorwho1986@foxmail.com>
2015-02-16 14:47:10 +0800

    BlockingQueue Collection is a type of Java Queue.  * BlockingQueue is part of Java concurrent util package.  * It is best used in multi threading and producer-consumer scenarios.  * While adding an element to a a BlockingQueue,  * if there is no space it can wait till it becomes available.  * Similarly while retrieving, it will wait till an element is available if it is empty.

---
+++ java-concurrent/src/main/java/com/doctor/concurrent/blockingqueue/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * @see http://javapapers.com/java/java-blockingqueue/
+ * 
+ * BlockingQueue Collection is a type of Java Queue. 
+ * BlockingQueue is part of Java concurrent util package. 
+ * It is best used in multi threading and producer-consumer scenarios.
+ * While adding an element to a a BlockingQueue, 
+ * if there is no space it can wait till it becomes available. 
+ * Similarly while retrieving, it will wait till an element is available if it is empty. 
+ * @author doctor
+ *
+ * @time 2015年2月16日 下午2:43:51
+ */
+package com.doctor.concurrent.blockingqueue;
