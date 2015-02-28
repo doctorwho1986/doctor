d2d9251c6c4800fb86d23afd1e80d2a639191c2c
doctorwho1986 <doctorwho1986@foxmail.com>
2015-02-28 16:13:24 +0800

    PropertySourceAnnotation

--- springmvc-practice/src/test/java/com/doctor/spring/annotation/importresource/PropertySourceConfig.java
+++ springmvc-practice/src/test/java/com/doctor/spring/annotation/importresource/PropertySourceConfig.java
@@ -1,7 +1,6 @@
 package com.doctor.spring.annotation.importresource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
