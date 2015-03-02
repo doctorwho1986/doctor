5565f8245c9dbcdfd3ae1234ae7cd21163c40134
doctorwho1986 <doctorwho1986@foxmail.com>
2015-03-03 00:17:38 +0800

    @ComponentScan(basePackages={"com.doctor.spring4.manager"})

--- spring4-2015/src/main/java/com/doctor/spring4/config/RootConfig.java
+++ spring4-2015/src/main/java/com/doctor/spring4/config/RootConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages={""})
+@ComponentScan(basePackages={"com.doctor.spring4.manager"})
 public class RootConfig {
 
 }
