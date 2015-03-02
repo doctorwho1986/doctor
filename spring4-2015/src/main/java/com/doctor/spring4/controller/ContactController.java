8a89832449638a0ac86735a1f896741493542f3f
doctorwho1986 <doctorwho1986@foxmail.com>
2015-03-03 00:33:19 +0800

    ContactController

---
+++ spring4-2015/src/main/java/com/doctor/spring4/controller/ContactController.java
@@ -0,0 +1,27 @@
+package com.doctor.spring4.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.doctor.spring4.manager.ContactManager;
+
+/**
+ * 
+ * @author doctor
+ *
+ * @since 2015年3月3日 上午00:32:49
+ */
+@Controller
+public class ContactController {
+	
+	@Autowired
+	private ContactManager contactManager;
+
+	@RequestMapping("/contacts")
+	public String contacts(Model model) {
+		model.addAttribute("contacts", contactManager.getContacts());
+		return "contacts";
+	}
+}
