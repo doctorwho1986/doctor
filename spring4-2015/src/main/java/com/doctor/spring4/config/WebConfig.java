1d5bc5d03976e226f6f5a59a26587d0c097b9bdd
doctorwho1986 <doctorwho1986@foxmail.com>
2015-03-02 21:25:40 +0800

    Thymeleaf + Spring　配置

--- spring4-2015/src/main/java/com/doctor/spring4/config/WebConfig.java
+++ spring4-2015/src/main/java/com/doctor/spring4/config/WebConfig.java
@@ -9,19 +9,24 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+import org.thymeleaf.templateresolver.TemplateResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = { "com.doctor.spring4.controller" })
 public class WebConfig extends WebMvcConfigurerAdapter {
 
-	@Bean
-	public ViewResolver viewResolver() {
-		InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
-		internalResourceViewResolver.setPrefix("/WEB-INF/jsp/");
-		internalResourceViewResolver.setSuffix(".jsp");
-		return internalResourceViewResolver;
-	}
+//	@Bean
+//	public ViewResolver viewResolver() {
+//		InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
+//		internalResourceViewResolver.setPrefix("/WEB-INF/jsp/");
+//		internalResourceViewResolver.setSuffix(".jsp");
+//		return internalResourceViewResolver;
+//	}
 
 	@Override
 	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
@@ -34,4 +39,28 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 		registry.addViewController("/").setViewName("index");
 	}
 
+	
+	@Bean
+	public TemplateResolver templateResolver(){
+		TemplateResolver templateResolver = new ServletContextTemplateResolver();
+		templateResolver.setPrefix("/WEB-INF/html/");
+		templateResolver.setSuffix(".html");
+		templateResolver.setTemplateMode("HTML5");
+		return templateResolver;
+	}
+	
+	@Bean
+	public SpringTemplateEngine templateEngine(TemplateResolver templateResolver){
+		SpringTemplateEngine springTemplateEngine = new SpringTemplateEngine();
+		springTemplateEngine.setTemplateResolver(templateResolver);
+		return springTemplateEngine;
+	}
+	
+	@Bean
+	public ViewResolver viewResolver(SpringTemplateEngine templateEngine){
+		ThymeleafViewResolver thymeleafViewResolver = new ThymeleafViewResolver();
+		thymeleafViewResolver.setTemplateEngine(templateEngine);
+		thymeleafViewResolver.setCharacterEncoding("utf-8");//解决页面中文乱码
+		return thymeleafViewResolver;
+	}
 }
