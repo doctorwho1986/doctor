328a2143d8add232cafd42b7f2082dede6beaab5
doctorwho1986 <doctorwho1986@foxmail.com>
2015-03-02 21:46:52 +0800

    add CharacterEncodingFilter

--- spring4-2015/src/main/java/com/doctor/spring4/config/SpittrWebAppInitializer.java
+++ spring4-2015/src/main/java/com/doctor/spring4/config/SpittrWebAppInitializer.java
@@ -1,5 +1,8 @@
 package com.doctor.spring4.config;
 
+import javax.servlet.Filter;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class SpittrWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
@@ -17,6 +20,14 @@ public class SpittrWebAppInitializer extends AbstractAnnotationConfigDispatcherS
 	@Override
 	protected String[] getServletMappings() {
 		return new String[] { "/" };
+	}
+
+	@Override
+	protected Filter[] getServletFilters() {
+		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+		characterEncodingFilter.setEncoding("utf-8");
+		characterEncodingFilter.setForceEncoding(true);
+		return new  Filter[]{characterEncodingFilter};
 	}
 
 }
