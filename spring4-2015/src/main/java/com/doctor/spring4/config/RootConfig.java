117e7750494969e1a33183eedadbb1a04af3950e
doctorwho1986 <doctorwho1986@foxmail.com>
2015-03-03 15:00:17 +0800

    root spring 配置

--- spring4-2015/src/main/java/com/doctor/spring4/config/RootConfig.java
+++ spring4-2015/src/main/java/com/doctor/spring4/config/RootConfig.java
@@ -2,9 +2,17 @@ package com.doctor.spring4.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
+/**
+ * root spring 配置
+ * @author doctor
+ *
+ * @time 2015年3月2日 下午2:58:40
+ */
 @Configuration
-@ComponentScan(basePackages={"com.doctor.spring4.manager"})
+@ComponentScan(basePackages = { "com.doctor.spring4.manager" })
+@Import({ DataSourceConfig.class })
 public class RootConfig {
 
 }
