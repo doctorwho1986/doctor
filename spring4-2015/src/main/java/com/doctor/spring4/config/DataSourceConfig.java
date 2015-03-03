a4bb2d217feb87a61c1e93e81ba584d7e0d10acd
doctorwho1986 <doctorwho1986@foxmail.com>
2015-03-03 14:58:15 +0800

    数据源配置

---
+++ spring4-2015/src/main/java/com/doctor/spring4/config/DataSourceConfig.java
@@ -0,0 +1,15 @@
+package com.doctor.spring4.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 数据源配置
+ * 
+ * @author doctor
+ *
+ * @time 2015年3月3日 下午2:57:10
+ */
+@Configuration
+public class DataSourceConfig {
+
+}
