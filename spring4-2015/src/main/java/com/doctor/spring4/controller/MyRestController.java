14e5683038e2a7250e1c3753e08d7b6f0ad71cf6
doctorwho1986 <doctorwho1986@foxmail.com>
2015-03-04 15:54:50 +0800

    * java反射不会得到方法参数@PathVariable　注解的此参数的名字，spring如何做到的呢，详见： 	 * {@link org.springframework.core.LocalVariableTableParameterNameDiscoverer} 	 * 利用字节码技术得到的．mybatis注解的mapper是采用java 反射技术，多个参数必须给注解提供具体变量名值．

--- spring4-2015/src/main/java/com/doctor/spring4/controller/MyRestController.java
+++ spring4-2015/src/main/java/com/doctor/spring4/controller/MyRestController.java
@@ -4,8 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * rest 请求
+ * 
  * @author doctor
  *
  * @time 2015年3月3日 下午3:12:27
@@ -21,6 +23,15 @@ public class MyRestController {
 		return "movie";
 	}
 
+	/**
+	 * java反射不会得到方法参数@PathVariable　注解的此参数的名字，spring如何做到的呢，详见：
+	 * {@link org.springframework.core.LocalVariableTableParameterNameDiscoverer}
+	 * 利用字节码技术得到的．mybatis注解的mapper是采用java 反射技术，多个参数必须给注解提供具体变量名值．
+	 * 
+	 * @param movieName
+	 * @param modelMap
+	 * @return
+	 */
 	@RequestMapping("/{movieName}/a.json")
 	public String getMovie(@PathVariable String movieName, ModelMap modelMap) {
 		modelMap.addAttribute("movie", movieName);
