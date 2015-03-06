4c8fb7f3551bd3107e7fdde350a70a4d88e29b59
doctorwho1986 <doctorwho1986@foxmail.com>
2015-03-06 11:24:10 +0800

    InstantFormat

--- java-practice/src/test/java/com/github/jdk8/InstantFormat.java
+++ java-practice/src/test/java/com/github/jdk8/InstantFormat.java
@@ -4,7 +4,6 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoField;
 import java.time.temporal.UnsupportedTemporalTypeException;
 
 import org.junit.Rule;
