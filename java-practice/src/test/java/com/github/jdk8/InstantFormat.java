c6b4537ad3b5e12da178595fd871325816a30233
doctorwho1986 <doctorwho1986@foxmail.com>
2015-03-06 11:20:33 +0800

    InstantFormat

---
+++ java-practice/src/test/java/com/github/jdk8/InstantFormat.java
@@ -0,0 +1,45 @@
+package com.github.jdk8;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoField;
+import java.time.temporal.UnsupportedTemporalTypeException;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+/**
+ * @see http://stackoverflow.com/questions/25229124/format-instant-to-string
+ * @author doctor
+ *
+ * @time 2015年3月6日 上午11:17:54
+ */
+public class InstantFormat {
+
+	@Rule
+	public ExpectedException ex = ExpectedException.none();
+
+	@Test
+	public void test_wrong_format() {
+		Instant now = Instant.now();
+		ex.expect(UnsupportedTemporalTypeException.class);
+		String format = DateTimeFormatter.ofPattern("yyyy:MM:dd HH:mm:ss").format(now);
+		// java.time.temporal.UnsupportedTemporalTypeException: Unsupported field: YearOfEra
+		// at java.time.Instant.getLong(Instant.java:608)
+	}
+
+	/**
+	 * You can not use DateTimeFormatter to format an instant. Instead you must format it by hand.
+	 * One way to do this is to convert the Instant to a LocalDateTime
+	 */
+	@Test
+	public void test_right_format() {
+		LocalDateTime localDateTime = LocalDateTime.ofInstant(Instant.now(), ZoneId.systemDefault());
+		String format = localDateTime.format(DateTimeFormatter.ofPattern("yyyy:MM:dd HH:mm:ss"));
+		System.out.println(format);
+	}
+
+}
