cc17a7b2d608b55f7064736d55e6b6847545b4f3
doctorwho1986 <doctorwho1986@foxmail.com>
2015-03-10 17:38:12 +0800

    UserMapperTest

---
+++ spring4-2015/src/test/java/com/doctor/spring4/common/mapper/UserMapperTest.java
@@ -0,0 +1,53 @@
+package com.doctor.spring4.common.mapper;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.core.IsEqual.*;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.doctor.spring4.test.config.TestConfig;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = { TestConfig.class })
+public class UserMapperTest {
+
+	@Resource
+	private UserMapper userMapper;
+
+	@Resource(name = "Dbh2JdbcTemplate")
+	private JdbcTemplate jdbcTemplate;
+	
+	private String tableName = "spring4_2015_user";
+	private String table = "\"" + tableName + "\"";
+
+	@Test
+	public void testCreateUserTable() {
+		
+		jdbcTemplate.execute("drop table if exists " + table );
+		userMapper.createUserTable(tableName);
+		Integer count  = jdbcTemplate.queryForObject("select count(1)  from information_schema.tables t where t.table_name = '" + tableName + "\'", Integer.class); 
+		assertThat(count, equalTo(1));
+	}
+	
+	@Test
+	public void test_existTable_no(){
+		jdbcTemplate.execute("drop table if exists " + table );
+		Integer count  = jdbcTemplate.queryForObject("select count(1)  from information_schema.tables t where t.table_name = '" + tableName + "\'", Integer.class); 
+		assertThat(count, equalTo(0));
+	}
+	
+	@Test
+	public void test_existTable_yes(){
+		Integer count  = jdbcTemplate.queryForObject("select count(1)  from information_schema.tables t where t.table_name = 'TABLES'", Integer.class); 
+		int num = userMapper.existTable("TABLES");
+		assertThat(count, equalTo(num));
+		assertThat(num, equalTo(1));
+	}
+
+}
