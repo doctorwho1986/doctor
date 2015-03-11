b38b34db83f524b8eef18f8473ff69c15cfde6f5
doctorwho1986 <doctorwho1986@foxmail.com>
2015-03-11 13:43:28 +0800

    @ImportResource({"classpath:/spring4_2015Config/springweb-filter.xml"})

--- spring4-2015/src/main/java/com/doctor/spring4/config/RootConfig.java
+++ spring4-2015/src/main/java/com/doctor/spring4/config/RootConfig.java
@@ -3,6 +3,7 @@ package com.doctor.spring4.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
 
 /**
  * root spring 配置
@@ -13,6 +14,7 @@ import org.springframework.context.annotation.Import;
 @Configuration
 @ComponentScan(basePackages = { "com.doctor.spring4.manager" })
 @Import({ DataSourceConfig.class })
+@ImportResource({"classpath:/spring4_2015Config/springweb-filter.xml"})
 public class RootConfig {
 
 }
