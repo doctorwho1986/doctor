4c0f792d5bf905585a1d03ca7d68181c8a7a9273
doctorwho1986 <doctorwho1986@foxmail.com>
2015-03-11 17:44:54 +0800

    * 注意：MybatisH2Config　类中两个有依赖关系的bean注入方法，不要用set方法形式注入，  * 有可能导致注入循环引用问题．（@MapperScan　注解在MybatisH2Config，而且注解依赖里面的bean定义）．

--- spring4-2015/src/main/java/com/doctor/spring4/config/DataSourceConfig.java
+++ spring4-2015/src/main/java/com/doctor/spring4/config/DataSourceConfig.java
@@ -6,7 +6,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import javax.annotation.Resource;
 import javax.sql.DataSource;
 
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -25,6 +24,8 @@ import com.alibaba.druid.pool.DruidDataSource;
  * 数据源配置
  * 数据源配置个人觉得还是xml好些。用xml配置改动增加配置只需重启
  * 
+ * 注意：MybatisH2Config　类中两个有依赖关系的bean注入方法，不要用set方法形式注入，
+ * 有可能导致注入循环引用问题．（@MapperScan　注解在MybatisH2Config，而且注解依赖里面的bean定义）．
  * @author doctor
  *
  * @time 2015年3月3日 下午2:57:10
@@ -55,6 +56,7 @@ public class DataSourceConfig {
 	 * @time 2015年3月3日 下午3:26:15
 	 */
 	@Configuration
+	@MapperScan(basePackages = { "com.doctor.spring4.common.mapper" }, annotationClass = DbH2.class, sqlSessionFactoryRef = "dbH2SqlSessionFactory")
 	@PropertySource("classpath:/spring4_2015Pro/jdbc-H2.properties")
 	static class MybatisH2Config {
 
@@ -134,36 +136,15 @@ public class DataSourceConfig {
 		}
 
 		@Bean(name = "dbH2SqlSessionFactory")
-		@Resource(name = "dbH2DataSource")
-		public SqlSessionFactory DbH2SqlSessionFactory(DataSource dataSource) throws Exception {
+		public SqlSessionFactory DbH2SqlSessionFactory() throws Exception {
 			SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
 
-			sqlSessionFactoryBean.setDataSource(dataSource);
+			sqlSessionFactoryBean.setDataSource(dbH2DataSource());
 			sqlSessionFactoryBean.setTypeHandlersPackage("");
 
 			sqlSessionFactoryBean.setConfigLocation(new ClassPathResource("/spring4_2015Config/mybatis-db-config.xml"));
 			return sqlSessionFactoryBean.getObject();
 		}
-
-	}
-
-	@Configuration
-	@MapperScan(basePackages = { "com.doctor.spring4.common.mapper" }, annotationClass = DbH2.class, sqlSessionFactoryRef = "dbH2SqlSessionFactory")
-	static class MyBatisMapperConfig {
-		// MapperScan注解不要和数据源定义的配置写在一起，（如MybatisH2Config配置上），
-		// 否此会导致循环引用初始化bean问题．
-		// 看来xml配置还是有优势的
-
-		// 03-11 17:01:50.010 main WARN o.s.b.f.s.DefaultListableBeanFactory - Bean creation
-		// exception on FactoryBean type check:
-		// org.springframework.beans.factory.BeanCreationException: Error creating bean with name
-		// 'userMapper' defined in file
-		// [/home/cui/workspace/spring4-2015/target/classes/com/doctor/spring4/common/mapper/UserMapper.class]:
-		// Cannot resolve reference to bean 'dbH2SqlSessionFactory' while setting bean property
-		// 'sqlSessionFactory'; nested exception is
-		// org.springframework.beans.factory.BeanCurrentlyInCreationException: Error creating bean
-		// with name 'dbH2SqlSessionFactory': Requested bean is currently in creation: Is there an
-		// unresolvable circular reference?
 
 	}
 }
