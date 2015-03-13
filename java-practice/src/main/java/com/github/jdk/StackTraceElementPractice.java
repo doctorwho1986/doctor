b2f3814e9b403e4dccef91297be477e3c39d3dc6
doctorwho1986 <doctorwho1986@foxmail.com>
2015-03-13 09:18:37 +0800

    * Java如何取源文件中文件名和行号  *  * {@code StackTraceElement}的定义详见文档

---
+++ java-practice/src/main/java/com/github/jdk/StackTraceElementPractice.java
@@ -0,0 +1,24 @@
+package com.github.jdk;
+
+/**
+ * Java如何取源文件中文件名和行号
+ * 
+ * {@code StackTraceElement}的定义详见文档
+ * 
+ * @author doctor
+ *
+ * @time 2015年3月13日 上午9:16:16
+ */
+public class StackTraceElementPractice {
+
+	public static void main(String[] args) {
+
+		StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+		System.out.println(stackTrace.length);
+		StackTraceElement stackTraceElement = stackTrace[stackTrace.length - 1];
+		System.out.println(stackTraceElement.getFileName());
+		System.out.println(stackTraceElement.getLineNumber());
+
+	}
+
+}
