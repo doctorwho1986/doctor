ace97f14c1168086fc75a32ce8b5208573e91d5b
doctorwho1986 <doctorwho1986@foxmail.com>
2015-03-14 16:05:45 +0800

    Getting Started with Google Guava ebook

---
+++ openSourcLibrary-2015/src/test/java/com/doctor/guava/ebook/getting_started_with_google_guava/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Getting Started with Google Guava ebook
+ * 
+ * @author doctor
+ *
+ * @since 2015年3月14日 上午11:35:22
+ */
+package com.doctor.guava.ebook.getting_started_with_google_guava;
---
+++ openSourcLibrary-2015/src/test/java/com/doctor/guava/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * guava类库探索
+ * 
+ * @author doctor
+ *
+ * @since 2015年3月14日 上午11:30:25
+ */
+package com.doctor.guava;
